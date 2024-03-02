56a1947742bca69197296e26452124a629cbe0f3
은냥 <instru93@naver.com>
2024-03-02 20:41:49 +0900

    초기커밋

---
+++ googleBooks/googleBooks/supportFiles/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  googleBooks
+//
+//  Created by 김현지 on 3/2/24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
