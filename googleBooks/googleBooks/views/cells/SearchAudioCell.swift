f8e7c686cc68b9c8697d7d88cedef108897638e6
은냥 <instru93@naver.com>
2024-03-05 01:33:19 +0900

    셀 생성 및 커밋

---
+++ googleBooks/googleBooks/views/cells/SearchAudioCell.swift
@@ -0,0 +1,22 @@
+//
+//  SearchAudioCell.swift
+//  googleBooks
+//
+//  Created by 김현지 on 3/4/24.
+//
+
+import UIKit
+
+class SearchAudioCell: UITableViewCell {
+    
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
