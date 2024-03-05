c8af3b63ae0e542fadf5b90204d57131a981d67f
은냥 <instru93@naver.com>
2024-03-05 10:37:28 +0900

    디테일뷰 생성

---
+++ googleBooks/googleBooks/views/DetailView.swift
@@ -0,0 +1,29 @@
+//
+//  DetailView.swift
+//  googleBooks
+//
+//  Created by 김현지 on 3/5/24.
+//
+
+import UIKit
+
+class DetailView: BaseView {
+    
+    
+    
+    override func initView() {
+        super.initView()
+    }
+    
+    override func initModel() {
+        super.initModel()
+    }
+    
+    override func firstAction() {
+        super.firstAction()
+    }
+    
+    override func viewReloadAction() {
+        super.viewReloadAction()
+    }
+}
