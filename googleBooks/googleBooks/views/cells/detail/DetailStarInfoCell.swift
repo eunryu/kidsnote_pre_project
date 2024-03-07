92a915357b49c1c0a5f45a915307f639932bf5dd
은냥 <instru93@naver.com>
2024-03-07 23:03:58 +0900

    평점셀 화살표 제거

--- googleBooks/googleBooks/views/cells/detail/DetailStarInfoCell.swift
+++ googleBooks/googleBooks/views/cells/detail/DetailStarInfoCell.swift
@@ -10,7 +10,6 @@ import UIKit
 class DetailStarInfoCell: BaseTableViewCell {
     
     var titleLabel: UILabel!
-    var arrowV: UIImageView!
     var ratingBox: UIView!
     
     var ratingLabel: UILabel!
@@ -22,8 +21,6 @@ class DetailStarInfoCell: BaseTableViewCell {
         
         titleLabel = MakeUILabelKit.shared.makeLabel("평점 및 리뷰", size: CGSize(width: 150, height: 25), addView: self)
         MakeUILabelKit.shared.textDecoration(titleLabel, fontSize: 20, fontName: nil, color: .blk333, alignment: .left)
-
-        arrowV = MakeUIImageViewKit.shared.makeImageView(image: UIImage(resource: .iconArrowRight), size: CGSize(width: 16, height: 16), addView: self)
         
         ratingBox = MakeUIViewKit.shared.makeView(size: CGSize(width: 300, height: 50), background: .white)
         self.addSubview(ratingBox)
@@ -32,12 +29,8 @@ class DetailStarInfoCell: BaseTableViewCell {
         autoKit.setAutoLayout(16, Trailing: 16, Top: nil, Bottom: 10, Width: nil, Height: 50, TargetView: ratingBox, MainView: self)
         autoKit.setViewTerm(16, topView: titleLabel, bottomView: ratingBox, mainView: self)
         
-        autoKit.setEqualWidthAndHeight(16, height: 16, targetView: arrowV)
-        autoKit.equalViewCenterVertical(targetView: arrowV, standardView: titleLabel, mainView: self)
-        autoKit.Trailing(arrowV, MainView: self, TrailingSize: 16)
-        
         // 레이팅바
-        ratingLabel = MakeUILabelKit.shared.makeLabel("5.0", size: CGSize(width: 80, height: 30), addView: ratingBox)
+        ratingLabel = MakeUILabelKit.shared.makeLabel("0.0", size: CGSize(width: 80, height: 30), addView: ratingBox)
         MakeUILabelKit.shared.textDecoration(ratingLabel, fontSize: 20, fontName: nil, color: .blk333, alignment: .left)
         
         ratingCntLabel = MakeUILabelKit.shared.makeLabel("Google Play 평점 0개", size: CGSize(width: 100, height: 20), addView: ratingBox)
